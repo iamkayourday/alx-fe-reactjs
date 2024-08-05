78b72c3d897898c3a12126c172f1c4207b96a459
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-08-05 06:01:17 +0100

    website modified

---
+++ alx-react-app/src/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
