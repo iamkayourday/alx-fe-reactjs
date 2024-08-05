b37b3da3e36cf9c885979f6be3b8e0021705f155
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-08-05 07:58:40 +0100

    code updated so the checker can pass it, although everything is okay

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -2,9 +2,9 @@ const UserProfile = (props) => {
 
     return(
         <div>
-            <h2>{props.name}</h2>
-            <p>{props.age}</p>
-            <p>{props.bio}</p>
+            <h2>Name: {props.name}</h2>
+            <p>Age: {props.age} years</p>
+            <p>Bio: {props.bio}</p>
         </div>
     )
 }
