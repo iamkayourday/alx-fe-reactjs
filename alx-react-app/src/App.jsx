7ec330c44a0bedb8188f6eb58fc76b2d5cf39e5c
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-08-05 07:38:11 +0100

    component added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import WelcomeMessage from "./components/WelcomeMessage"
-import Header from './Header';
-import MainContent from "./MainContent"
-import Footer from "./Footer"
+import Header from './components/Header';
+import MainContent from "./components/MainContent"
+import Footer from "./components/Footer"
 
 
 
