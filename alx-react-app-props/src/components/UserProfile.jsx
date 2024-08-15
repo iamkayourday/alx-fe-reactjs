9b0d49697d11e4b9aa3281770bab71266c4b7e5f
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-08-15 11:46:15 +0100

    Unnecessary codes removed

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,18 +1,3 @@
-// import { createContext } from "react"
-// import { useContext } from "react"
-
-// const UserProfile = () => {
-// const UserProfile = createContext();
-//     return(
-//         <div>
-//             <h2>Name: {name}</h2>
-//             <p>Age: {age} years</p>
-//             <p>Bio: {bio}</p>
-//         </div>
-//     )
-// }
-// export default UserProfile
-// src/components/UserProfile.jsx
 import { useContext } from "react";
 import UserContext from "../components/UserContext";
 
