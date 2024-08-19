868832a567c90e8e655f6cff5ea67f67a9d48371
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-08-19 09:01:43 +0100

    recipeStore file deleted cos it is not pushing to github after it was renamed

--- recipe-sharing-app/src/recipestore.js
+++
@@ -1,9 +0,0 @@
-import create from 'zustand';
-
-const useRecipeStore = create(set => ({
-  recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
-}));
-
-export { useRecipeStore };
