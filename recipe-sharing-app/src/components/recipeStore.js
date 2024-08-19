761841c499f3a116ae41856c7320257a15c42990
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-08-19 09:03:06 +0100

    recipeStore file added

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,9 @@
+import create from 'zustand';
+
+const useRecipeStore = create(set => ({
+  recipes: [],
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes })
+}));
+
+export { useRecipeStore };
