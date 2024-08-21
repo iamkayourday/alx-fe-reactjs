bd68b714c7ade950fe7f3badd73acf57be712c1f
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-08-21 04:14:40 +0100

    Navigation required in DeleteRecipeButton, so when a recipe is deleted the browser redirectts the user to the homepage

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import { useRecipeStore } from './recipeStore';
+
 const DeleteRecipeButton = ({recipeId }) => {
+const navigate = useNavigate();
 const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
 
 const handleDelete = () => {
   deleteRecipe(recipeId)
-}
+  navigate('/');
+};
   return (
     <button onClick={handleDelete}>
       Delete Recipe
