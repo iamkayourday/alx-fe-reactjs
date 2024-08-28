9ab349f2ca57cb97049d8a58faac6b2340cb0008
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-08-28 16:37:06 +0100

    PostsComponent component modified to catch error and a button was added to refetch posts from the api, which updates the posts in the UI if there is any changes in the API

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { useQuery } from 'react-query'
 
-function PostsComponent() {
+const fetchPosts = async () => {
+    console.log('Fetching posts...');
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    return response.json();
+    if (response) {
+        return <div>Fetching posts...</div>
+    }
+}
+
+const PostsComponent = () => {
+    const {data, isLoading, isError, error} = useQuery('fetchData', fetchPosts);
+    if (isLoading) {
+        return <h1>Loading...</h1>
+    }
+    if (isError) {
+        return <h1>Error loading data</h1>
+    }
+
   return (
     <>
-        PostsComponent
+       <h2>Fetch data using react-query</h2> 
+       <button onClick={fetchPosts}>Refetch Posts</button>
+       <div>
+       <ol>
+            {data.map(post => (
+                <li key={post.id}>{post.title}</li>
+            ))}
+        </ol>
+       </div>
     </>
   )
 }
