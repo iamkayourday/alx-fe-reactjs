b07d32b2dcb1bcab8fdef373ef8fa7121f0b0ca7
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-08-28 17:07:22 +0100

    implemented CatchTime, StaleTime, refetchOnWindow and KeepPreviousData to the component to improve performnace

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,37 +1,43 @@
 import React from 'react'
-import { useQuery } from 'react-query'
+import { useQuery, useQueryClient } from 'react-query'
 
 const fetchPosts = async () => {
-    console.log('Fetching posts...');
     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    console.log('fetching Posts...')
     return response.json();
-    if (response) {
-        return <div>Fetching posts...</div>
-    }
+  
 }
 
 const PostsComponent = () => {
-    const {data, isLoading, isError, error} = useQuery('fetchData', fetchPosts);
+    const queryClient = useQueryClient();
+
+    const { data, isLoading, isError, error } = useQuery('posts', fetchPosts, {
+        cacheTime: 1000 * 60 * 5, 
+        staleTime: 1000 * 60 * 1, 
+        refetchOnWindowFocus: true,
+        keepPreviousData: true,
+    });
+
     if (isLoading) {
         return <h1>Loading...</h1>
     }
     if (isError) {
-        return <h1>Error loading data</h1>
+        return <h1>Error loading data: {error.message}</h1>
     }
 
-  return (
-    <>
-       <h2>Fetch data using react-query</h2> 
-       <button onClick={fetchPosts}>Refetch Posts</button>
-       <div>
-       <ol>
-            {data.map(post => (
-                <li key={post.id}>{post.title}</li>
-            ))}
-        </ol>
-       </div>
-    </>
-  )
+    return (
+        <>
+            <h2>Fetch data using react-query</h2> 
+            <button onClick={() => queryClient.invalidateQueries('posts')}>Refetch Posts</button>
+            <div>
+                <ol>
+                    {data.map(post => (
+                        <li key={post.id}>{post.title}</li>
+                    ))}
+                </ol>
+            </div>
+        </>
+    );
 }
 
-export default PostsComponent
+export default PostsComponent;
