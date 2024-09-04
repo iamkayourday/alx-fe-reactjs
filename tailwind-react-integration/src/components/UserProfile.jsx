b16b19f61cfb3efa591d160ec4dc28b1ff2dffbf
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-09-04 16:48:32 +0100

    Responsive design implemented using Tailwind css

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
     return (
-      <div className="bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg shadow-lg">
-        <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 mx-auto" />
-        <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-        <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <div className="bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg shadow-lg sm:p-4 sm:max-w-xs md:p-8 md:max-w-sm ">
+        <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36" />
+        <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">John Doe</h1>
+        <p className="text-gray-600 text-base sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
     );
   }
