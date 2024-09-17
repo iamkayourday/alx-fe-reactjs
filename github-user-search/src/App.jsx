971e6d2525593dbec5793cf952a69e6b9437119c
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-09-17 18:34:37 +0100

    Github User Searc component implemented, Axios insatlled too

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
