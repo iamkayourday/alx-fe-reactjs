4e7c12e77bce73f58834a5f6280e03d4459e568b
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-09-17 19:22:34 +0100

    Renders Search component

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Search from './components/Search';
 
 function App() {
 
   return (
     <>
+    <div>
+      <h1>Search GitHub Like a Pro!</h1>
+      <Search />
+    </div>
       
     </>
   )
