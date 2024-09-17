7cffbb7e03a5f7e65299d173791dcb1957c811b3
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-09-17 17:58:09 +0100

    Update

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -18,7 +18,7 @@ const RecipeDetail = () => {
       </div>
     );
   }
-
+  
   return (
     <div className="max-w-4xl mx-auto p-6 bg-white shadow-md rounded-lg">
       <div className="flex flex-col items-center md:flex-row md:space-x-8">
