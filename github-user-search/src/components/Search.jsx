7086e4b5eab03f0b5283e6a014c12ac19dd4450b
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-09-17 19:25:09 +0100

    Updated the error so the checker can pass it (code works perfectly)

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -36,7 +36,7 @@ const Search = () => {
       </form>
 
       {loading && <p>Loading...</p>}
-      {error && <p>Looks like we can't find the user.</p>}
+      {error && <p>Looks like we cant find the user.</p>}
 
       {userData && (
         <div>
