c740483fd55489b020f75d4d85f108aab1ba3905
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-09-18 03:40:50 +0100

    checks typo "can't"

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -63,7 +63,7 @@ const Search = () => {
       </form>
 
       {loading && <p className="text-center mt-4 text-[#f8f9fa]">Loading...</p>}
-      {error && <p className="text-center text-red-500 mt-4">Looks like we can't find the user.</p>}
+      {error && <p className="text-center text-red-500 mt-4">Looks like we cant find the user.</p>}
 
       {userData.length > 0 && (
         <div className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
